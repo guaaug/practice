b2494adf3270f4fe92a596a92b7a268c642b5e64
guaaug <343656537@qq.com>
2019-07-26 08:42:19 +0800

    add leetcode solution 41 1st missing positive

---
+++ src/main/java/com/yuanstone/practice/leetcode/Solution0041.java
@@ -0,0 +1,41 @@
+package com.yuanstone.practice.leetcode;
+
+/**
+ * 给定一个未排序的整数数组，找出其中没有出现的最小的正整数。
+ * <p>
+ * 示例 1:
+ * 输入: [1,2,0]
+ * 输出: 3
+ * <p>
+ * 示例 2:
+ * 输入: [3,4,-1,1]
+ * 输出: 2
+ * <p>
+ * 示例 3:
+ * 输入: [7,8,9,11,12]
+ * 输出: 1
+ * <p>
+ * 说明: 你的算法的时间复杂度应为O(n)，并且只能使用常数级别的空间。
+ */
+public class Solution0041 {
+
+    public static void main (String[] args) {
+        int[] nums = {3, 4, -1, 1};
+        System.out.println(firstMissingPositive(nums));
+    }
+
+    public static int firstMissingPositive (int[] nums) {
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            while (nums[i] > 0 && nums[i] <= length && nums[i] != i + 1 && nums[nums[i] - 1] != nums[i]) {
+                int temp = nums[i];
+                nums[i] = nums[temp - 1];
+                nums[temp - 1] = temp;
+            }
+        }
+        for (int i = 0; i < length; i++) {
+            if (nums[i] != i + 1) return i + 1;
+        }
+        return length + 1;
+    }
+}
