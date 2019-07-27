703a65af63d321ea3b4db32455d3751c347461bf
guaaug <343656537@qq.com>
2019-07-27 18:24:04 +0800

    add leetcode solution 121 max profit

---
+++ src/main/java/com/yuanstone/practice/leetcode/solution200/Solution0121.java
@@ -0,0 +1,53 @@
+package com.yuanstone.practice.leetcode.solution200;
+
+/**
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ * 如果你最多只允许完成一笔交易（即买入和卖出一支股票），设计一个算法来计算你所能获取的最大利润。
+ * 注意你不能在买入股票前卖出股票。
+ * <p>
+ * 示例 1:
+ * 输入: [7,1,5,3,6,4]
+ * 输出: 5
+ * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+ * 注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
+ * <p>
+ * 示例 2:
+ * 输入: [7,6,4,3,1]
+ * 输出: 0
+ * 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+ */
+public class Solution0121 {
+
+    public static void main (String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println(maxProfit(prices));
+    }
+
+    public static int maxProfit (int[] prices) {
+        if (prices == null || prices.length <= 1) return 0;
+        int min = prices[0];
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            } else {
+                profit = Math.max(profit, prices[i] - min);
+            }
+        }
+        return profit;
+    }
+
+    // 另一个解法
+    public int maxProfit2 (int[] prices) {
+        int n = prices.length;
+        // base case: dp[-1][0] = 0, dp[-1][1] = -infinity
+        int dp_i_0 = 0, dp_i_1 = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            // dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
+            dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+            // dp[i][1] = max(dp[i-1][1], -prices[i])
+            dp_i_1 = Math.max(dp_i_1, -prices[i]);
+        }
+        return dp_i_0;
+    }
+}
